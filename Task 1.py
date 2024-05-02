e2c79910d26696ac9a5fa3de600f47ba62f156fc
Uniwait <evgen.kushnir99@gmail.com>
2024-05-02 21:51:18 +0300

    Add Task 1 implementation

---
+++ Task 1.py
@@ -0,0 +1,21 @@
+arr = [65, 3, 65]
+
+def Great(arr):
+    try:
+        if arr[0]**2 + arr[1]**2 == arr[2]**2:
+            return True
+        elif arr[0]**2 + arr[2]**2 == arr[1]**2:
+            return True
+        elif arr[1]**2 + arr[2]**2 == arr[0]**2:
+            return True
+        else:
+            return False
+    except IndexError:
+        print("Error: There are not enough elements in the array to perform the operation.")
+        return False
+    except TypeError:
+        print("Error: Numeric data type expected."
+)
+
+result = Great(arr)
+print(result)
